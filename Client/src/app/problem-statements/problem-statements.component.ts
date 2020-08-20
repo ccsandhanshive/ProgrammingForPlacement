6d24e27190e85e9101178806cd5972746afcbca2
ccsandhanshive <ccsandhanshive2014@gmail.com>
2020-08-20 19:18:36 +0530

    problem statements and programming window component added

--- Client/src/app/problem-statements/problem-statements.component.ts
+++ Client/src/app/problem-statements/problem-statements.component.ts
@@ -8,17 +8,41 @@ import { ProblemStatementsService } from '../problem-statements.service';
 export class ProblemStatementsComponent implements OnInit {
 
   constructor(private ProblemStatementsService:ProblemStatementsService) { }
-  problems:any
+  problems:any=[]
+  problem:any
+  id:1
+  Ava=false
+
+  setId(id){
+    this.id=id
+    this.setProblem()
+  }
   ngOnInit(): void {
+    this.Ava=false
     this.problems=this.getProblems()
+    console.log(this.problem)
+  
+    
+  }
+  tempProblem:any
+  setProblem(){
+    for(let p of this.problems){
+      if(p.problemId==this.id){
+        this.problem=p
+        this.Ava=true
+      }
+    }
   }
   getProblems(){
     return this.ProblemStatementsService.getProblems()
     .subscribe(
-      problems => {
-       console.log(this.problems);
-       this.problems = problems
-      }
-     );
+      problems => this.problems = problems
+       );
   }
+
+ /* setProblemDetails(object){
+    this.problem=object.problem
+    this.compilarLink=object.compilarLink
+    this.problemVisibility=true
+  }*/
 }
